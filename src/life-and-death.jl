6a0406da5253c91c6d98b732062d1b86fc0a9487
evalparse <zhuojia.dai@gmail.com>
2022-07-11 10:39:07 +1000

    Update life-and-death.jl

--- src/life-and-death.jl
+++ src/life-and-death.jl
@@ -1,1 +1,12 @@
 # functions for solving life and death problems
+
+
+function solve_ld(board::LDBoard, playable_area = get_playable_area(board))
+  # the playable_area should be a list of coordindate indicating possible positions of play
+
+  for move in playable_area
+    if can_move(board, move)
+      solve_ld(board, playable_area)
+    end
+  end
+end
